afd02a6ff5b9dc5c12d527929b46180e1dd1d1bb
piyush3216 <piyushchourey6079@gmail.com>
2022-01-06 03:48:38 +0530

    Updates

---
+++ precache-manifest.1db459d012677799bfd6d8088cfe61ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40da5c19019776881a7b",
+    "url": "/textUtils/static/css/main.2ef56cd0.chunk.css"
+  },
+  {
+    "revision": "40da5c19019776881a7b",
+    "url": "/textUtils/static/js/main.40da5c19.chunk.js"
+  },
+  {
+    "revision": "09f7f987b5e710312800",
+    "url": "/textUtils/static/js/1.09f7f987.chunk.js"
+  },
+  {
+    "revision": "37911c2b0274c69b02f3",
+    "url": "/textUtils/static/js/2.37911c2b.chunk.js"
+  },
+  {
+    "revision": "57efad6a6a876bd82722",
+    "url": "/textUtils/static/js/runtime~main.57efad6a.js"
+  },
+  {
+    "revision": "df554c664af15dcaf06730ab9c1b4443",
+    "url": "/textUtils/index.html"
+  }
+];
